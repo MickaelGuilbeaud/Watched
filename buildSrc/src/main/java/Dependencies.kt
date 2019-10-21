06501c868a7b7f7903849a454682517173359a37
Mickaël Guilbeaud <MickaelGuilbeaud@users.noreply.github.com>
2019-10-21 12:17:57 +0200

    Bumped gradle and libraries versions

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,16 +5,16 @@
 object Versions {
     const val kotlin = "1.3.50"
     const val lifecycle = "2.1.0"
-    const val okhttp = "4.1.1"
-    const val retrofit = "2.6.1"
-    const val glide = "4.9.0"
-    const val dagger = "2.24"
+    const val okhttp = "4.2.2"
+    const val retrofit = "2.6.2"
+    const val moshi = "1.8.0"
+    const val glide = "4.10.0"
 }
 
 object Dependencies {
     // Kotlin
     const val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
-    const val kotlinCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.1"
+    const val kotlinCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.2"
 
     // AndroidX - General
     const val activity = "androidx.activity:activity-ktx:1.0.0"
@@ -26,17 +26,18 @@ object Dependencies {
 
     // AndroidX - UI
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta2"
-    const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0-beta04"
+    const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0-beta05"
 
     // AndroidX - Lifecycle
-    const val lifecycle = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"   // ViewModel and LiveData
+    const val lifecycle =
+        "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"   // ViewModel and LiveData
     const val livedata =
         "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"  // Extensions functions for LiveData
     const val viewModel =
         "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"    // Extensions functions for ViewModel
 
     // UI
-    const val material = "com.google.android.material:material:1.1.0-alpha10"
+    const val material = "com.google.android.material:material:1.1.0-beta01"
     const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
     const val glideProcessor = "com.github.bumptech.glide:compiler:${Versions.glide}"
     const val glideOkhttp3 = "com.github.bumptech.glide:okhttp3-integration:${Versions.glide}"
@@ -46,14 +47,18 @@ object Dependencies {
     const val okHttpLogging = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
     const val retrofitClient = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
     const val retrofitAdapterRxJava2 = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    const val retrofitMoshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+
+    // JSON
+    const val moshi = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
 
     // Rx
-    const val rxJava = "io.reactivex.rxjava2:rxjava:2.2.12"
+    const val rxJava = "io.reactivex.rxjava2:rxjava:2.2.13"
     const val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
     const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:2.4.0"
 
     // DI
-    const val koin = "org.koin:koin-android:2.0.1"
+    const val koin = "org.koin:koin-android-scope:2.0.1"
 
     // Firebase
     const val firebaseCore = "com.google.firebase:firebase-core:17.2.0"
