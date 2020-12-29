19bca8773e84993f4bc70c8baac5833ad2d4d412
Mickaël Guilbeaud <MickaelGuilbeaud@users.noreply.github.com>
2020-12-29 17:43:59 +0100

    [Animes] Removed Toolbar as the screen name is now visible in the BottomNavigationView. Updated RecyclerView margins so the fab doesn't cover content when the view is scrolled to the bottom, and the content doesn't start at the top of the screen.

--- design/src/main/java/mg/watched/design/MarginItemDecoration.kt
+++ design/src/main/java/mg/watched/design/MarginItemDecoration.kt
@@ -7,9 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 class MarginItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.apply {
-            top = spaceHeight
-            bottom = spaceHeight
-        }
+        outRect.bottom = spaceHeight
     }
 }
