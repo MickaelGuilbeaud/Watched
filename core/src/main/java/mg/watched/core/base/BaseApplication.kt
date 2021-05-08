15e00a652b3451127e17d2358cc8576415fa7d5d
mickael <mickael.guilbeaud@gmail.com>
2021-05-08 17:51:59 +0200

    Removed unused product flavors.

--- core/src/main/java/mg/watched/core/base/BaseApplication.kt
+++ core/src/main/java/mg/watched/core/base/BaseApplication.kt
@@ -2,8 +2,6 @@ package mg.watched.core.base
 
 import android.app.Application
 import mg.watched.core.BuildConfig
-import mg.watched.core.build.BuildEnvironment
-import mg.watched.core.build.BuildParams
 import mg.watched.core.utils.CrashlyticsLogsTree
 import mg.watched.core.utils.CrashlyticsWatchedExceptionsTree
 import timber.log.Timber
@@ -13,8 +11,6 @@ abstract class BaseApplication : Application() {
     companion object {
         lateinit var instance: BaseApplication
     }
-
-    abstract val buildParams: BuildParams
 
     override fun onCreate() {
         super.onCreate()
@@ -27,10 +23,11 @@ abstract class BaseApplication : Application() {
     abstract fun initDI()
 
     private fun initLogging() {
-        if (buildParams.environment != BuildEnvironment.DEV) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
             Timber.plant(CrashlyticsLogsTree())
             Timber.plant(CrashlyticsWatchedExceptionsTree())
         }
-        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
--- core/src/main/java/mg/watched/core/build/BuildConfig.kt
+++ core/src/main/java/mg/watched/core/build/BuildConfig.kt
@@ -1,10 +1,5 @@
 package mg.watched.core.build
 
-interface BuildParams {
-    val baseUrl: String
-    val environment: BuildEnvironment
-}
-
 enum class BuildEnvironment {
     DEV,
     PROD,
