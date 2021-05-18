466f32ba85fce7c1eb82921aaedbd28d241768b7
Mickaël Guilbeaud <MickaelGuilbeaud@users.noreply.github.com>
2021-05-18 22:01:21 +0200

    Update dependencies versions (#11)
    * Bumped Kotlin version to 1.5 and various dependencies versions. Also completely removed jcenter from the project.
    
    * Removed unused product flavors.
    
    * Removed Room as it was not used.

--- app/src/main/java/mg/watched/WatchedApplication.kt
+++ app/src/main/java/mg/watched/WatchedApplication.kt
@@ -2,7 +2,6 @@ package mg.watched
 
 import mg.watched.animes.animes.animesDiModule
 import mg.watched.core.base.BaseApplication
-import mg.watched.core.build.BuildParams
 import mg.watched.data.di.dataDiModule
 import mg.watched.di.appDiModule
 import mg.watched.login.loginDiModule
@@ -12,8 +11,6 @@ import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class WatchedApplication : BaseApplication() {
-
-    override val buildParams: BuildParams = BuildParamsImpl()
 
     override fun initDI() {
         startKoin {
